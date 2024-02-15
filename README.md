941b4b806f0af8debf0b81fd07aeac8e8f8ea5d4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-02-15 14:54:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a temporary repository to highligh the issue with Composer >= 2.7, issue that was not present in Composer < 2.6.
+
+The issue has been opened on the Composer repository: https://github.com/composer/composer/pull/11850
+
+To run the reproducer script, run the `./run.sh` script
---
+++ run.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Exit on error
+set -e
+
+# To run this bash script, you need to have the following tools installed:
+# php, composer, git, jq
+
+# Set the path to the composer executable (if not in PATH)
+# This script fails with Composer >= 2.7
+COMPOSER=composer
+
+rm -rf vendor composer.lock composer.json
+cp composer.json.orig composer.json
+cp composer.lock.orig composer.lock
+
+export COMPOSER_CACHE_DIR=/dev/null
+export COMPOSER_MINIMAL_CHANGES=1
+export COMPOSER_DISABLE_NETWORK=0
+
+echo "Using composer version..."
+$COMPOSER --version
+
+echo "Validating composer.json and composer.lock files..."
+$COMPOSER validate --no-ansi --no-interaction
+
+echo "Prevent the use of packagist.org"
+$COMPOSER config repo.packagist false
+echo "Modifying composer.json to use the local composer repository..."
+composer config repo.composer composer file://$(pwd)/local-repo/packages.json
+
+echo "Disabling network access..."
+export COMPOSER_DISABLE_NETWORK=1
+export COMPOSER_MIRROR_PATH_REPOS=1
+
+echo "Updating composer.lock file since we modified the composer.json file..."
+$COMPOSER \
+    --lock \
+    --no-ansi \
+    --no-install \
+    update
+
+echo "Validating composer.json and composer.lock files..."
+$COMPOSER validate --no-ansi --no-interaction
+
+echo "Installing the dependencies..."
+$COMPOSER \
+    --no-ansi \
+    --no-interaction \
+    install
